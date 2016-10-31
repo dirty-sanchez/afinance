29f1ae2e394b305680c5d6392f79a9f92358ae3b
dirty-sanchez <sas.seven@gmail.com>
2016-10-31 18:10:56 +0400

    fixed statistics

--- assets/js/reports/controller.js
+++ assets/js/reports/controller.js
@@ -26,31 +26,85 @@ angular.module('app.reports', [])
     $scope.fetchData = (filter) => {
       DetailsReport.query(filter, (queryData) => {
         var columnsInfo;
-        var report = {
-          columnsInfo: {},
-          rowsInfo: {},
-          data: {}
+        var rangeInfo = {
+          _data: {},
+          collectValue: (rangeId, label, valueKey, value) => {
+            var range;
+            if (rangeInfo._data[rangeId] == undefined) {
+              rangeInfo._data[rangeId] = {id: rangeId, label: label, values: {}};
+            }
+
+            range = rangeInfo._data[rangeId];
+            if (range.values[valueKey] == undefined) {
+              range.values[valueKey] = value;
+            } else {
+              range.values[valueKey] += value;
+            }
+          }
         };
 
-        var rowsInfo = {};
-        queryData.forEach((dataItem) => {
-          var recordId = 'position:' + dataItem.positionId;
-          if (rowsInfo[recordId] === undefined) {
-            rowsInfo[recordId] = {
-              id: recordId,
-              label: dataItem.positionName,
-              values: {}
-            };
-          }
+        var rowOrderInfo = { items: [{id: 'cost', label: 'Расходы', items: []}, {id: 'income', label: 'Доходы', items: []}] };
 
+        function addPositionInfo (hierarchyList) {
+          var hierarchyElement = rowOrderInfo;
+          for (var idx in hierarchyList) {
+            var foundEl = hierarchyElement.items.find((el) => {
+              if (el.id === hierarchyList[idx].id) {
+                return el;
+              }
+            });
+
+            if (foundEl == undefined) {
+              foundEl = {id: hierarchyList[idx].id, label: hierarchyList[idx].label, items: []};
+              hierarchyElement.items.push(foundEl);
+            }
+
+            hierarchyElement = foundEl;
+          }
+        }
+
+        queryData.forEach((dataItem) => {
+          var positionRangeId = 'position:' + dataItem.positionId;
+          var posGroupRangeId = 'group:' + dataItem.groupId;
+          var costTypeRangeLabel = (dataItem.costType === 'income') ? 'Доходы' : 'Расходы';
           var valueKey = makeValueKeyFromDateStr(dataItem.date).toISOString();
-          rowsInfo[recordId].values[valueKey] = dataItem.value;
+          rangeInfo.collectValue(positionRangeId, dataItem.positionName, valueKey, dataItem.value);
+          rangeInfo.collectValue(posGroupRangeId, dataItem.groupName, valueKey, dataItem.value);
+          rangeInfo.collectValue(dataItem.costType , costTypeRangeLabel, valueKey, dataItem.value);
+          addPositionInfo([
+            {id: dataItem.costType, label: costTypeRangeLabel},
+            {id: posGroupRangeId, label: dataItem.groupName},
+            {id: positionRangeId, label: dataItem.positionName}
+          ]);
         });
+
+        var orderedRowsInfo = [];
+        rowOrderInfo.items.forEach((costType) => {
+          var values = (rangeInfo._data[costType.id] != null) ? rangeInfo._data[costType.id].values : [];
+
+          orderedRowsInfo.push({ id: costType.id, label: costType.label, values: values, offset: 0 });
+          costType.items.sort((el1, el2) => {
+            return el1.label.localeCompare(el2.label);
+          });
+
+          costType.items.forEach((positionGroup) => {
+            var values = (rangeInfo._data[positionGroup.id] != null) ? rangeInfo._data[positionGroup.id].values : [];
+
+            orderedRowsInfo.push({ id: positionGroup.id, label: positionGroup.label, values: values, offset: 1 });
+            positionGroup.items.sort((el1, el2) => {
+              return el1.label.localeCompare(el2.label);
+            }).forEach((position) => {
+              var values = (rangeInfo._data[position.id] != null) ? rangeInfo._data[position.id].values : [];
+
+              orderedRowsInfo.push({ id: position.id, label: position.label, values: values, offset: 2 });
+              });
+          });
+        });
+
         columnsInfo = prepareColumnHeaders($scope.filter);
         $scope.report = {
           columnsInfo: columnsInfo,
-          rowsInfo: rowsInfo,
-          // data: positionDataMap
+          rowsInfo: orderedRowsInfo
         };
       });
     }
@@ -64,7 +118,7 @@ function incDate(currentDate, groupBy) {
   var res = new Date(currentDate);
   switch (groupBy) {
     case 'year':
-      return new Date(currentDate.getFullYear() + 1);
+      return new Date(currentDate.getFullYear() + 1, currentDate.getMonth());
     case 'month':
       res.setMonth(currentDate.getMonth() + 1);
       return res;
@@ -82,7 +136,7 @@ function formatDateLabel(date, groupBy) {
   var lbl;
   switch (groupBy) {
     case 'month':
-      lbl = monthMap[date.getMonth()] + ' - ' + date.getYear();
+      lbl = monthMap[date.getMonth()] + ' - ' + date.getFullYear();
       break;
     case 'year':
       lbl = date.getFullYear();
@@ -103,7 +157,7 @@ function prepareColumnHeaders(f) {
       id: dateCntr.toISOString(),
       label: formatDateLabel(dateCntr, f.groupBy),
     });
-    dateCntr = incDate(dateCntr);
+    dateCntr = incDate(dateCntr, f.groupBy);
     if (--cycleLimit === 0) {
       throw Error('cycleLimit reached 0. halting');
     }
